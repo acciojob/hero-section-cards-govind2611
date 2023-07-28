38bd1c9b7bf4107e94fe3e1ba2bd7ac30aae3ef0
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-28 08:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+// JavaScript can be used to handle the comment submission and display dummy comments.
+
+// Function to handle comment submission
+function submitComment() {
+  const commentInput = document.getElementById("comment-input");
+  const commentText = commentInput.value.trim();
+
+  if (commentText !== "") {
+    // Create a new comment element
+    const newComment = document.createElement("div");
+    newComment.className = "comment";
+    newComment.textContent = commentText;
+
+    // Append the new comment to the comments section
+    const commentsSection = document.querySelector(".comments");
+    commentsSection.appendChild(newComment);
+
+    // Clear the comment input
+    commentInput.value = "";
+  }
+}
+
+// Event listener for comment submission
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", submitComment);
